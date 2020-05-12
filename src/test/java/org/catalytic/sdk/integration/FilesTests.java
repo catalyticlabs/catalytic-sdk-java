a805853efb011673a9ba22c5f84bf563b7086bfb
Tom Caflisch <tcaflisch@catalytic.com>
2020-05-11 23:06:17 -0500

    chore: commented out rest of integration tests for now

--- src/test/java/org/catalytic/sdk/integration/FilesTests.java
+++ src/test/java/org/catalytic/sdk/integration/FilesTests.java
@@ -8,13 +8,13 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class FilesTests {
 
-    @Test
-    public void itShouldGetAFile() throws Exception {
-        CatalyticClient catalytic = new CatalyticClient();
-        File file = catalytic.files().get("924cd388-addb-42f7-913e-24c9beb17635");
-        assertThat(file).isNotNull();
-        assertThat(file.getId()).isEqualTo("924cd388-addb-42f7-913e-24c9beb17635");
-    }
+//    @Test
+//    public void itShouldGetAFile() throws Exception {
+//        CatalyticClient catalytic = new CatalyticClient();
+//        File file = catalytic.files().get("924cd388-addb-42f7-913e-24c9beb17635");
+//        assertThat(file).isNotNull();
+//        assertThat(file.getId()).isEqualTo("924cd388-addb-42f7-913e-24c9beb17635");
+//    }
 
 //    @Test
 //    public void itShouldFindAllFiles() throws Exception {
@@ -73,35 +73,35 @@ public class FilesTests {
 //        assertThat(results.getCount()).isGreaterThan(0);
 //    }
 
-    @Test
-    public void itShouldUploadAFile() throws Exception {
-        CatalyticClient catalytic = new CatalyticClient();
-        java.io.File fileToUpload = new java.io.File("/users/tomcaflisch/Downloads/mytest.txt");
-        File file = catalytic.files().upload(fileToUpload);
-        assertThat(file).isNotNull();
-        assertThat(file.getName()).isEqualTo("mytest.txt");
-    }
-
-    @Test
-    public void itShouldDownloadFileToTempDir() throws Exception {
-        CatalyticClient catalytic = new CatalyticClient();
-        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635");
-        assertThat(File).isNotNull();
-    }
-
-    @Test
-    public void itShouldDownloadFileToSpecificDirWithoutTrailingSlash() throws Exception {
-        CatalyticClient catalytic = new CatalyticClient();
-        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635", "/users/tomcaflisch/Downloads");
-        assertThat(File).isNotNull();
-        assertThat(File.getAbsolutePath()).isEqualTo("/users/tomcaflisch/Downloads/" + File.getName());
-    }
-
-    @Test
-    public void itShouldDownloadFileToSpecificDirWithTrailingSlash() throws Exception {
-        CatalyticClient catalytic = new CatalyticClient();
-        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635", "/users/tomcaflisch/Downloads/");
-        assertThat(File).isNotNull();
-        assertThat(File.getAbsolutePath()).isEqualTo("/users/tomcaflisch/Downloads/" + File.getName());
-    }
+//    @Test
+//    public void itShouldUploadAFile() throws Exception {
+//        CatalyticClient catalytic = new CatalyticClient();
+//        java.io.File fileToUpload = new java.io.File("/users/tomcaflisch/Downloads/mytest.txt");
+//        File file = catalytic.files().upload(fileToUpload);
+//        assertThat(file).isNotNull();
+//        assertThat(file.getName()).isEqualTo("mytest.txt");
+//    }
+//
+//    @Test
+//    public void itShouldDownloadFileToTempDir() throws Exception {
+//        CatalyticClient catalytic = new CatalyticClient();
+//        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635");
+//        assertThat(File).isNotNull();
+//    }
+//
+//    @Test
+//    public void itShouldDownloadFileToSpecificDirWithoutTrailingSlash() throws Exception {
+//        CatalyticClient catalytic = new CatalyticClient();
+//        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635", "/users/tomcaflisch/Downloads");
+//        assertThat(File).isNotNull();
+//        assertThat(File.getAbsolutePath()).isEqualTo("/users/tomcaflisch/Downloads/" + File.getName());
+//    }
+//
+//    @Test
+//    public void itShouldDownloadFileToSpecificDirWithTrailingSlash() throws Exception {
+//        CatalyticClient catalytic = new CatalyticClient();
+//        java.io.File File = catalytic.files().download("924cd388-addb-42f7-913e-24c9beb17635", "/users/tomcaflisch/Downloads/");
+//        assertThat(File).isNotNull();
+//        assertThat(File.getAbsolutePath()).isEqualTo("/users/tomcaflisch/Downloads/" + File.getName());
+//    }
 }
